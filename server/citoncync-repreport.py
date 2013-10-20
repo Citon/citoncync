3d730476efb59f7fa9707ad54e40a940a8297f11
pauldoom <paul.hirsch@citon.com>
2013-10-20 12:33:26 -0500

    * Python is installed on FreeNAS - Take advantage for our reporting script

---
+++ server/citoncync-repreport.py
@@ -0,0 +1,10 @@
+#!python
+
+# Copyright 2013, Citon Computer Corporation
+# Author: Paul Hirsch
+
+# citoncync-repreport - Scan a given "homebase" directory for one or more
+#                       CitonCync customer subdirectories then report to
+#                       one or more email addresses on last sync, data usage
+#                       and other fun facts.
+
